fa4318533f15830208254b441a5a23b34f90575e
LeejunHyeok402 <wnsgur970718@gmail.com>
2022-09-27 20:01:25 +0900

    0927 python

---
+++ Hello_pyton/day01/if02.py
@@ -0,0 +1,8 @@
+a = 3
+
+if a > 5:
+    print("5보다큼")
+elif a > 3 :
+    print("3보다큼")
+elif a > 1 :
+    print("1보다큼")
---
+++ Hello_pyton/day02/mytest02.py
@@ -0,0 +1,15 @@
+# 출력원하는 단수를 입력하세요 5
+# 5*1=5
+# 5*2=10
+dan = input("출력원하는 단수를 입력하세요") 
+idan = int(dan)
+
+print("{}*{}={}".format(idan,1,idan*1))
+print("{}*{}={}".format(idan,2,idan*2))
+print("{}*{}={}".format(idan,3,idan*3))
+print("{}*{}={}".format(idan,4,idan*4))
+print("{}*{}={}".format(idan,5,idan*5))
+print("{}*{}={}".format(idan,6,idan*6))
+print("{}*{}={}".format(idan,7,idan*7))
+print("{}*{}={}".format(idan,8,idan*8))
+print("{}*{}={}".format(idan,9,idan*9))
